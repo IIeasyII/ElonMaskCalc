ced3ee0a19f269e65134e5d53c82680718aec5ed
easy <zavolokin.easy.igor@gmail.com>
2018-10-30 02:19:29 +0300

    Task calculate: -sum(int, double) -ras(int, double) -pow(int, double)

--- EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
+++ EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,60 @@ namespace EM.Calc.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Core.Calc calc = new Core.Calc();
+
+            var input = Console.ReadLine();
+
+            object array = splitArray(input);
+
+            object result = new object();
+
+            switch (input.Split(Convert.ToChar(" "))[0])
+            {
+                case "sum":
+                    result = calc.Sum(array);
+                    break;
+                case "ras":
+                    result = calc.Ras(array);
+                    break;
+                case "pow":
+                    result = calc.Pow(array);
+                    break;
+            }
+
+            Console.Write(Convert.ToString(result));
+            Console.ReadKey();
+        }
+
+        private static object splitArray(string str)
+        {
+            var array = str.Split(Convert.ToChar(" "));
+
+            ArrayList newArray = new ArrayList();
+
+            if (str.Contains(","))
+            {
+                for (int i = 1; i < array.Length; i++)
+                    newArray.Add((array[i]));
+                return doubleArray(newArray);
+            }
+            else
+            {
+                for (int i = 1; i < array.Length; i++)
+                    newArray.Add(Convert.ToInt32(array[i]));
+                return newArray.OfType<int>().ToArray();
+            }
+        }
+
+        private static double[] doubleArray(ArrayList array)
+        {
+            List<double> termsList = new List<double>();
+
+            foreach (object item in array)
+                termsList.Add(Convert.ToDouble(item));
+
+            return termsList.ToArray<double>();
         }
     }
+
 }
--- EM.Calc.Tests/CalcTest.cs
+++ EM.Calc.Tests/CalcTest.cs
@@ -6,11 +6,12 @@ namespace EM.Calc.Tests
     [TestClass]
     public class CalcTest
     {
+        Core.Calc calc = new Core.Calc();
+
         [TestMethod]
-        public void TestSum()
+        public void TestSumInt()
         {
             // arange
-            var calc = new Core.Calc();
             var sum = 10;
 
             // act
@@ -18,6 +19,71 @@ namespace EM.Calc.Tests
 
             // assert
             Assert.AreEqual(sum, result);
+        }
+
+        [TestMethod]
+        public void TestSumDouble()
+        {
+            // arange
+            var sum = 11.4;
+
+            // act
+            var result = (double)calc.Sum(new[] { 5.5, 3.1, 2.8 });
+
+            // assert
+            Assert.AreEqual(sum, result);
+        }
+
+        [TestMethod]
+        public void TestRasInt()
+        {
+            // arange
+            var ras = -13;
+
+            // act
+            var result = (int)calc.Ras(new[] { 4, 8, 9 });
+
+            // assert
+            Assert.AreEqual(ras, result);
+        }
+
+        [TestMethod]
+        public void TestRasDouble()
+        {
+            // arange
+            var ras = -15.2;
+
+            // act
+            var result = (double)calc.Ras(new[] { 3.2, 8.5, 9.9 });
+
+            // assert
+            Assert.AreEqual(ras, result);
+        }
+
+        [TestMethod]
+        public void TestPowInt()
+        {
+            // arange
+            var pow = 262144;
+
+            // act
+            var result = (int)calc.Pow(new[] { 2, 3, 6 });
+
+            // assert
+            Assert.AreEqual(pow, result);
+        }
+
+        [TestMethod]
+        public void TestPowDouble()
+        {
+            // arange
+            var pow = 626932771.13720214;
+
+            // act
+            var result = (double)calc.Pow(new[] { 2.3, 3.8, 6.4 });
+
+            // assert
+            Assert.AreEqual(pow, result);
         }
     }
 }
